bc3f8f706c5ab7fa8896703586738dc34cf2db55
Gustavo Jobstraibizer <gustavojobs_dev@outlook.com>
2020-08-07 19:41:14 -0300

    Ajustes em vendas por hora trocando R$ por % e alteracoes em data periodo dos filtros

--- src/app/shared/_components/filter/periodo/periodo.component.ts
+++ src/app/shared/_components/filter/periodo/periodo.component.ts
@@ -1,14 +1,10 @@
-import { AdministracaoService } from './../../../../core/services/administracao.service';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import {
-  BsLocaleService,
-  DatepickerDateCustomClasses,
-} from 'ngx-bootstrap/datepicker';
-import { defineLocale, ptBrLocale } from 'ngx-bootstrap/chronos';
-import * as moment from 'moment/moment';
-import { IListaFranqueadoPerfil } from 'src/app/shared/interfaces/lista-franqueado-perfil.interface';
-defineLocale('pt-br', ptBrLocale);
+import { Component, EventEmitter, OnInit, Output } from '@angular/core'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
+import * as moment from 'moment/moment'
+import { defineLocale, ptBrLocale } from 'ngx-bootstrap/chronos'
+import { BsLocaleService } from 'ngx-bootstrap/datepicker'
+import { IListaFranqueadoPerfil } from 'src/app/shared/interfaces/lista-franqueado-perfil.interface'
+defineLocale('pt-br', ptBrLocale)
 
 @Component({
   selector: 'ft-periodo',
@@ -16,46 +12,46 @@ defineLocale('pt-br', ptBrLocale);
   styleUrls: ['./periodo.component.scss'],
 })
 export class PeriodoComponent implements OnInit {
-  @Output() periodoFilterEmit = new EventEmitter();
+  @Output() periodoFilterEmit = new EventEmitter()
 
-  public formPeriodo: FormGroup;
-  public items: IListaFranqueadoPerfil[];
-  public submitted = false;
+  public formPeriodo: FormGroup
+  public items: IListaFranqueadoPerfil[]
+  public submitted = false
 
   constructor(private localeService: BsLocaleService) {
     this.formPeriodo = new FormGroup({
-      dataInicio: new FormControl(moment(new Date()).format('DD/MM/YYYY'), [
-        Validators.required,
-      ]),
-      dataFim: new FormControl(
-        moment(new Date()).add(1, 'M').format('DD/MM/YYYY'),
+      dataInicio: new FormControl(
+        moment(new Date()).subtract(1, 'M').format('DD/MM/YYYY'),
         [Validators.required]
       ),
+      dataFim: new FormControl(moment(new Date()).format('DD/MM/YYYY'), [
+        Validators.required,
+      ]),
       codigoFranqueado: new FormControl(null, [Validators.required]),
-    });
+    })
   }
 
   ngOnInit(): void {
-    this.localeService.use('pt-br');
+    this.localeService.use('pt-br')
   }
 
   handleFilter() {
-    this.submitted = true;
+    this.submitted = true
     if (!this.formPeriodo.valid) {
-      return;
+      return
     }
 
-    this.submitted = false;
+    this.submitted = false
 
-    const { dataInicio, dataFim, codigoFranqueado } = this.formPeriodo.value;
+    const { dataInicio, dataFim, codigoFranqueado } = this.formPeriodo.value
     this.periodoFilterEmit.emit({
       dataInicio: moment(dataInicio, 'DD/MM/YYYY').format('MM/DD/YYYY'),
       dataFim: moment(dataFim, 'DD/MM/YYYY').format('MM/DD/YYYY'),
       codigoFranqueado,
-    });
+    })
   }
 
   isFieldValid(field: string) {
-    return !this.formPeriodo.get(field).valid && this.submitted;
+    return !this.formPeriodo.get(field).valid && this.submitted
   }
 }
